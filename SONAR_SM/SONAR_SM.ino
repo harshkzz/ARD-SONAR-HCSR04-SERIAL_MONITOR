05a4ca5214a129ff7ba01b8cd35497ba3b88e1fd
Harsh Kumar <125388784+harshkzz@users.noreply.github.com>
2023-04-08 14:17:42 +0530

    Add files via upload

---
+++ SONAR_SM/SONAR_SM.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#define echoPin 7                           // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
+#define trigPin 8                           // CHANGE PIN NUMBER HERE IF YOU WANT TO USE A DIFFERENT PIN
+long duration, distance;
+void setup(){
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+void loop(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration / 58.2;
+  String disp = String(distance);
+
+  Serial.print("distance :");
+  Serial.print(disp);
+  Serial.print(" cm");
+  Serial.println("");
+  delay(1000);
+}
